bd965b9add874b4d4f6919424580e66d16e9cc7e
Kartike <64118528+OzyKartike@users.noreply.github.com>
2023-11-03 18:41:50 -0400

    Add files via upload
    DDL files

---
+++ DDL_Files/HOSPITALSTAFFMAINTENANCE.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------
+--  DDL for View HOSPITALSTAFFMAINTENANCE
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE VIEW "AKIBRIA"."HOSPITALSTAFFMAINTENANCE" ("HOSPITALID", "STAFF_NAME", "OCCUPATION") AS 
+  SELECT
+    h.HOSPITALID,
+    s.NAME AS STAFF_NAME,
+    m.OCCUPATION
+FROM
+    HOSPITAL h
+INNER JOIN
+    STAFF s
+ON
+    h.HOSPITALID = s.HOSPITALID
+INNER JOIN
+    MAINTENANCE m
+ON
+    s.EMPLOYEEID = m.EMPLOYEEID
+;
---
+++ DDL_Files/PATIENTHEALTHRECORDS.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  DDL for View PATIENTHEALTHRECORDS
+--------------------------------------------------------
+
+  CREATE OR REPLACE FORCE VIEW "AKIBRIA"."PATIENTHEALTHRECORDS" ("FIRSTNAME", "LASTNAME", "HEALTHCARDNUMBER", "VITALINFO") AS 
+  SELECT
+    p.FIRSTNAME,
+    p.LASTNAME,
+    p.HEALTHCARDNUMBER,
+    hr.VITALINFO
+FROM
+    PATIENT p
+INNER JOIN
+    HEALTH_RECORD hr
+ON
+    p.HEALTHCARDNUMBER = hr.HEALTHCARDNUMBER
+;
